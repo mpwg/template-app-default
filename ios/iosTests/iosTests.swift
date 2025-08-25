d33b7f455a5f78a1cf7a6ba548b0973c27974803
Matthias Wallner-Géhri <68284225+mpwg@users.noreply.github.com>
2025-08-25 19:01:53 +0200

    add templates for iOS and Android

---
+++ ios/iosTests/iosTests.swift
@@ -0,0 +1,16 @@
+//
+//  iosTests.swift
+//  iosTests
+//
+//  Created by Matthias Wallner-Géhri on 25.08.25.
+//
+
+import Testing
+
+struct iosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
